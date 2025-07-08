b42038dc9109f1c552eaceb97dcbeb914ed86cb3
Marquardtt <gabrielropelatomarquardt@gmail.com>
2025-07-08 16:33:54 -0300

    Feature: Adding logo

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,5 +1,14 @@
+"use client"
+
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
+
 export const HeaderComponent = () => {
+    const router = useRouter()
     return (
-        <header className="fixed top-0 left-0 w-full h-20 bg-black" ></header>
+        <header className="w-full h-20 bg-black flex justify-center items-center">
+            <Image className="cursor-pointer" onClick={() => router.push("/")} src={"/temper_logo.png"} height={60} width={180} alt="Temper" />
+        </header>
     )
 }
